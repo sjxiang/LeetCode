159c0a4a26480311b7662203175ec64395d325a4
shengjiexiang <1535484943@qq.com>
2019-12-14 19:55:44 +0800

    leetcode 20

--- 031  下一个排列.py
+++
@@ -1,9 +0,0 @@
-""" 31. 下一个排列 """
-
-
-class Solution:
-    def nextPermutation(self, nums):
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        pass
---
+++ 20 有效的括号.py
@@ -0,0 +1,58 @@
+# leetcode 20. 有效的括号
+
+# 简单括号匹配
+
+# e.g.
+# 1  "()"、
+# 2  "()[]"
+# 3  "([]"
+# 4  ")("
+
+# 解法：栈
+# 开括号，压栈
+# 闭括号，先窥(None ?) 再判断
+
+class Solution:
+    def isValid(self, s):
+        stack = []
+        dict = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+
+        # 写法 1
+        # for c in s:
+        #     if c not in dict:
+        #         stack.append(c)
+        #     elif not stack or dict[c] != stack.pop():
+        #         return False
+        #
+        # return not stack
+
+
+        # 写法 2
+
+        index = 0
+        matched = True
+        while index < len(s) and matched:
+            char = s[index]
+            if char in dict:
+                stack.append(char)
+            else:
+                if stack is None:
+                    matched = False
+                else:
+                    stack.pop()
+
+            index += 1
+
+        if matched and not stack:
+            return True
+        else:
+            return False
+
+
+
+
+
